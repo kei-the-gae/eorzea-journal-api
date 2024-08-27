8d93d41f9dcb4f7c5f4c7d350b2462274c85b05e
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-08-27 13:01:18 -0700

    Add create character route

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 const User = require('../models/user');
 const Character = require('../models/character');
 const verifyToken = require('../middleware/verify-token');
 
+router.use(verifyToken);
 
+router.post('/', async (req, res) => {
+    try {
+        const character = await Character.create(req.body);
+        const user = await User.findById(req.params.userId);
+        user.characters.push(character._id);
+        await user.save();
+        res.status(201).json(character);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
 
 module.exports = router;
